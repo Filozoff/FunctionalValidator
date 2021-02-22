50befe1e18d62ce0201200c65f25ea1269d0394b
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2021-02-22 21:18:00 +0100

    Text field

--- FunctionalValidator/Source/Main/ViewController.swift
+++ FunctionalValidator/Source/Main/ViewController.swift
@@ -9,12 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	let textField = UITextField()
+
 	override func loadView() {
 		view = UIView()
 	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = .red
+
+		view.backgroundColor = .systemBackground
+		textField.placeholder = "Type number 0-1000"
+		textField.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(textField)
+
+		NSLayoutConstraint.activate([
+			textField.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor),
+			textField.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor),
+			textField.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+		])
 	}
 }
